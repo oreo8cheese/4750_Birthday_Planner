741253702781cb74d5d211b16a0f87c7e94fcf96
oreo8cheese <111255462+oreo8cheese@users.noreply.github.com>
2025-05-08 16:07:55 -0700

    Update api_config.dart removed api key

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,4 +1,4 @@
 class APIConfig {
   // Replace this with your actual OpenAI API key
-  static const String openAIKey = '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';
+  static const String openAIKey = 'Replace with api key';
 } 
